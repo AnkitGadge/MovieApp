d5f74c72a397c82ea697c1238ff7d808d0313e70
AnkitGadge <99271017+AnkitGadge@users.noreply.github.com>
2023-08-06 12:08:17 +0530

    issue solved in url of the fetch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SearchIcon from "./search.svg";
 import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 // 36b0c289
-const API_URL = "http://www.omdbapi.com?apikey=36b0c289";
+const API_URL = "https://www.omdbapi.com?apikey=36b0c289";
 
 
 const App = () => {
